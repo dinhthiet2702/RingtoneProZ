b4b125a0bd44cc80cad37fd78db040823e9019f9
Thomas Ngo <thomas.ngo@suppapp.com>
2022-05-28 11:37:49 +0700

    add proj library

---
+++ RingtoneProLib/RingtoneProLib/RingtoneProLib.h
@@ -0,0 +1,18 @@
+//
+//  RingtoneProLib.h
+//  RingtoneProLib
+//
+//  Created by thiet on 28/5/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RingtoneProLib.
+FOUNDATION_EXPORT double RingtoneProLibVersionNumber;
+
+//! Project version string for RingtoneProLib.
+FOUNDATION_EXPORT const unsigned char RingtoneProLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RingtoneProLib/PublicHeader.h>
+
+
